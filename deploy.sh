7568fdbd555971b598b638cd692323a54305b96c
ash03lko <ashweryaverma@gmail.com>
2025-06-20 12:16:28 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -5,13 +5,14 @@ trap 'echo "❌ Deployment failed!"' ERR
 VM_NAME="web-server"
 ZONE="us-central1-a"
 
-OSLOGIN_USER="sa_110306364646498021096"
+# Use your valid OS Login user
+OSLOGIN_USER="ashweryaverma_gmail_com"
 
 echo "📦 Copying app files to VM..."
-gcloud compute scp --recurse app/ ${OSLOGIN_USER}@${VM_NAME}:/home/${OSLOGIN_USER}/app --zone ${ZONE}
+gcloud compute scp --tunnel-through-iap --recurse app/ ${OSLOGIN_USER}@${VM_NAME}:/home/${OSLOGIN_USER}/app --zone ${ZONE}
 
 echo "🚀 Running app on VM..."
-gcloud compute ssh ${OSLOGIN_USER}@${VM_NAME} --zone ${ZONE} --command "
+gcloud compute ssh --tunnel-through-iap ${OSLOGIN_USER}@${VM_NAME} --zone ${ZONE} --command "
   sudo pkill -f 'python3 /home/${OSLOGIN_USER}/app/main.py' || true
   nohup python3 /home/${OSLOGIN_USER}/app/main.py > app.log 2>&1 &
 "
