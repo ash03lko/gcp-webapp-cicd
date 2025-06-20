488183439f193964fd99ccae99fe92ee3881f8c4
ash03lko <ashweryaverma@gmail.com>
2025-06-20 10:36:17 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -6,10 +6,10 @@ ZONE="us-central1-a"
 VM_USER="ashweryaverma"
 
 echo "📦 Copying app files to VM..."
-gcloud compute scp --recurse app/ ${VM_USER}@${VM_NAME}:/home/${VM_USER}/app --zone ${ZONE}
+gcloud compute scp --tunnel-through-iap --recurse app/ ${VM_USER}@${VM_NAME}:/home/${VM_USER}/app --zone ${ZONE}
 
 echo "🚀 Running app on VM..."
-gcloud compute ssh ${VM_USER}@${VM_NAME} --zone ${ZONE} --command "
+gcloud compute ssh --tunnel-through-iap ${VM_USER}@${VM_NAME} --zone ${ZONE} --command "
   sudo pkill -f 'python3 /home/${VM_USER}/app/main.py' || true
   nohup python3 /home/${VM_USER}/app/main.py > app.log 2>&1 &
 "
