a2b6e7be5866cfe14445052224c0c23bc1b5d954
ash03lko <ashweryaverma@gmail.com>
2025-06-22 16:11:39 +0530

    Update test_deploy.py

--- test/test_deploy.py
+++ test/test_deploy.py
@@ -4,7 +4,11 @@ import os
 
 class TestDeploy(unittest.TestCase):
     def test_homepage(self):
-        app_url = os.getenv('APP_URL', 'http://localhost:8080')
+        # APP_URL should be set as environment variable in Cloud Build test step
+        app_url = os.getenv('APP_URL')
+        if not app_url:
+            self.fail("APP_URL environment variable not set")
+
         response = requests.get(app_url)
         self.assertEqual(response.status_code, 200, f"Expected 200 OK but got {response.status_code}")
 
