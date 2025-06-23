9f9845aef408522ec8e3c575eb057bc12dbfc1ab
ash03lko <ashweryaverma@gmail.com>
2025-06-23 16:44:49 +0530

    Create variables.tf

---
+++ terraform/variables.tf
@@ -0,0 +1,19 @@
+variable "project_id" {
+  type    = string
+  default = "graphite-store-463414-p2"
+}
+
+variable "region" {
+  type    = string
+  default = "us-central1"
+}
+
+variable "zone" {
+  type    = string
+  default = "us-central1-a"
+}
+
+variable "email_alert" {
+  type    = string
+  default = "ashweryaverma@gmail.com"
+}
