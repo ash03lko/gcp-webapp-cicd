6111d812ee2e0cf82addad3edc9ce41cae4a656c
ash03lko <ashweryaverma@gmail.com>
2025-06-23 14:00:55 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,23 +1,19 @@
 variable "project_id" {
   description = "GCP project ID"
   type        = string
-  default     = "graphite-store-463414-p2"
 }
 
 variable "region" {
   description = "GCP region"
   type        = string
-  default     = "us-central1"
 }
 
 variable "zone" {
   description = "GCP zone"
   type        = string
-  default     = "us-central1-a"
 }
 
 variable "email_alert" {
   description = "Email for CPU alert notifications"
   type        = string
-  default     = "ashweryaverma@gmail.com"
 }
