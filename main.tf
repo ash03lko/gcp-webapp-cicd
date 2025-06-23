1b0a4575db1ae5617cf626f5cdf0eeeee6550098
ash03lko <ashweryaverma@gmail.com>
2025-06-23 09:20:49 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,28 +1,31 @@
 provider "google" {
-  project = var.project_id
-  region  = var.region
-  zone    = var.zone
+  project = "graphite-store-463414-p2"
+  region  = "us-central1"
+  zone    = "us-central1-a"
 }
 
+# VPC
 resource "google_compute_network" "vpc_network" {
-  name                    = var.vpc_name
-  auto_create_subnetworks = false
+  name = "my-vpc"
 }
 
+# Public subnet
 resource "google_compute_subnetwork" "public_subnet" {
   name          = "public-subnet"
-  ip_cidr_range = var.public_subnet_cidr
-  region        = var.region
+  ip_cidr_range = "11.0.1.0/24"
+  region        = "us-central1"
   network       = google_compute_network.vpc_network.id
 }
 
+# Private subnet
 resource "google_compute_subnetwork" "private_subnet" {
   name          = "private-subnet"
-  ip_cidr_range = var.private_subnet_cidr
-  region        = var.region
+  ip_cidr_range = "10.0.2.0/24"
+  region        = "us-central1"
   network       = google_compute_network.vpc_network.id
 }
 
+# Allow HTTP/HTTPS
 resource "google_compute_firewall" "allow_http_https" {
   name    = "allow-http-https"
   network = google_compute_network.vpc_network.name
@@ -35,6 +38,7 @@ resource "google_compute_firewall" "allow_http_https" {
   source_ranges = ["0.0.0.0/0"]
 }
 
+# Allow SSH from IAP
 resource "google_compute_firewall" "allow_ssh_from_iap" {
   name    = "allow-ssh-from-iap"
   network = google_compute_network.vpc_network.name
@@ -48,12 +52,12 @@ resource "google_compute_firewall" "allow_ssh_from_iap" {
   target_tags   = ["iap-ssh-enabled"]
 }
 
+# VM instance
 resource "google_compute_instance" "web_server" {
   name         = "web-server"
-  machine_type = var.machine_type
-  zone         = var.zone
-
-  tags = ["http-server", "iap-ssh-enabled"]
+  machine_type = "e2-medium"
+  zone         = "us-central1-a"
+  tags         = ["http-server", "iap-ssh-enabled"]
 
   boot_disk {
     initialize_params {
@@ -64,7 +68,14 @@ resource "google_compute_instance" "web_server" {
   network_interface {
     network    = google_compute_network.vpc_network.id
     subnetwork = google_compute_subnetwork.public_subnet.id
-    access_config {}
+
+    access_config {
+      # ephemeral external IP
+    }
+  }
+
+  metadata = {
+    enable-oslogin = "TRUE"
   }
 
   metadata_startup_script = <<-EOT
@@ -83,18 +94,14 @@ resource "google_compute_instance" "web_server" {
     systemctl enable docker
     systemctl start docker
 
-    gcloud auth configure-docker ${var.region}-docker.pkg.dev --quiet
+    gcloud auth configure-docker us-central1-docker.pkg.dev --quiet
 
-    docker pull ${var.region}-docker.pkg.dev/${var.project_id}/my-repo/my-app:latest
-    docker run -d --name my-app -p 80:80 ${var.region}-docker.pkg.dev/${var.project_id}/my-repo/my-app:latest
+    docker pull us-central1-docker.pkg.dev/graphite-store-463414-p2/my-repo/my-app:latest
+    docker run -d --name my-app -p 80:80 us-central1-docker.pkg.dev/graphite-store-463414-p2/my-repo/my-app:latest
   EOT
 
   service_account {
     scopes = ["https://www.googleapis.com/auth/cloud-platform"]
-  }
-
-  metadata = {
-    enable-oslogin = "TRUE"
   }
 
   labels = {
@@ -102,15 +109,17 @@ resource "google_compute_instance" "web_server" {
   }
 }
 
+# Notification channel
 resource "google_monitoring_notification_channel" "email_channel" {
   display_name = "Email Alert"
   type         = "email"
   labels = {
-    email_address = var.email_alert
+    email_address = "ashweryaverma@gmail.com"
   }
   enabled = true
 }
 
+# Alerting policy
 resource "google_monitoring_alert_policy" "cpu_alert" {
   display_name = "High CPU Usage Alert"
   combiner     = "OR"
@@ -119,10 +128,11 @@ resource "google_monitoring_alert_policy" "cpu_alert" {
   conditions {
     display_name = "VM CPU > 80%"
     condition_threshold {
-      filter          = "resource.type=\"gce_instance\" AND resource.label.instance_id=\"${google_compute_instance.web_server.id}\" AND metric.type=\"compute.googleapis.com/instance/cpu/utilization\""
+      filter          = "metric.type=\"compute.googleapis.com/instance/cpu/utilization\" AND resource.label.instance_id=monitoring.regex.full_match(\"${google_compute_instance.web_server.instance_id}\")"
       comparison      = "COMPARISON_GT"
       threshold_value = 0.8
       duration        = "60s"
+
       aggregations {
         alignment_period   = "60s"
         per_series_aligner = "ALIGN_MEAN"
@@ -130,7 +140,9 @@ resource "google_monitoring_alert_policy" "cpu_alert" {
     }
   }
 
-  notification_channels = [google_monitoring_notification_channel.email_channel.id]
-  project               = var.project_id
+  notification_channels = [google_monitoring_notification_channel.email_channel.name]
 }
 
+output "web_server_external_ip" {
+  value = google_compute_instance.web_server.network_interface[0].access_config[0].nat_ip
+}
