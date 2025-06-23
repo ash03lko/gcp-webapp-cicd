84a42183944df3c7ebe61cb1f134e53832866675
ash03lko <ashweryaverma@gmail.com>
2025-06-23 12:19:01 +0530

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,4 @@
 output "web_server_external_ip" {
   description = "Public IP of the web server"
-  value       = google_compute_instance.web_server.network_interface[0].access_config[0].nat_ip
+  value       = google_compute_instance.public_web_server.network_interface[0].access_config[0].nat_ip
 }
