1c514c81cfbe55e4e5e7e7bbb9e0c6629ef6788f
ash03lko <ashweryaverma@gmail.com>
2025-06-23 16:44:14 +0530

    Create main.tf

---
+++ terraform/main.tf
@@ -0,0 +1,90 @@
+resource "google_compute_network" "vpc_network" {
+  name = "webapp-vpc"
+}
+
+resource "google_compute_subnetwork" "public_subnet" {
+  name          = "public-subnet"
+  ip_cidr_range = "10.0.1.0/24"
+  region        = var.region
+  network       = google_compute_network.vpc_network.id
+}
+
+resource "google_compute_subnetwork" "private_subnet" {
+  name          = "private-subnet"
+  ip_cidr_range = "10.0.2.0/24"
+  region        = var.region
+  network       = google_compute_network.vpc_network.id
+  private_ip_google_access = true
+}
+
+resource "google_compute_firewall" "allow_http_https" {
+  name    = "allow-http-https"
+  network = google_compute_network.vpc_network.name
+
+  allow {
+    protocol = "tcp"
+    ports    = ["80", "443"]
+  }
+
+  source_ranges = ["0.0.0.0/0"]
+  target_tags   = ["http-server", "https-server"]
+}
+
+resource "google_monitoring_notification_channel" "email" {
+  display_name = "Email Notification"
+  type         = "email"
+  labels = {
+    email_address = var.email_alert
+  }
+}
+
+resource "google_monitoring_alert_policy" "cpu_alert" {
+  display_name = "High CPU Alert"
+  combiner     = "OR"
+
+  conditions {
+    display_name = "VM CPU > 80%"
+    condition_threshold {
+      filter = "metric.type=\"compute.googleapis.com/instance/cpu/utilization\" AND resource.label.instance_id=\"${google_compute_instance.web_instance.instance_id}\""
+      duration = "60s"
+      comparison = "COMPARISON_GT"
+      threshold_value = 0.8
+      aggregations {
+        alignment_period   = "60s"
+        per_series_aligner = "ALIGN_MEAN"
+      }
+    }
+  }
+
+  notification_channels = [google_monitoring_notification_channel.email.id]
+  enabled = true
+}
+
+resource "google_compute_instance" "web_instance" {
+  name         = "web-server"
+  machine_type = "e2-micro"
+  zone         = var.zone
+
+  tags = ["http-server", "https-server"]
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-11"
+    }
+  }
+
+  network_interface {
+    subnetwork = google_compute_subnetwork.public_subnet.id
+    access_config {}
+  }
+
+  metadata_startup_script = <<-EOT
+    #!/bin/bash
+    sudo apt update
+    sudo apt install -y nginx google-fluentd
+    sudo systemctl enable nginx
+    sudo systemctl start nginx
+    sudo systemctl enable google-fluentd
+    sudo systemctl start google-fluentd
+  EOT
+}
