7b696bdb63c3735d02e3f1d953a3defa609ba95c
ash03lko <ashweryaverma@gmail.com>
2025-06-23 16:46:39 +0530

    Delete outputs.tf

--- outputs.tf
+++
@@ -1,9 +0,0 @@
-output "web_server_external_ip" {
-  description = "The external IP of the public web server"
-  value       = google_compute_instance.public_web_server.network_interface[0].access_config[0].nat_ip
-}
-
-output "web_server_internal_ip" {
-  description = "The internal IP of the public web server"
-  value       = google_compute_instance.public_web_server.network_interface[0].network_ip
-}
