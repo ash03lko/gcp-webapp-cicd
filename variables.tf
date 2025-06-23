411b24ee25d8f18792b6bf8ffb22396351676a19
ash03lko <ashweryaverma@gmail.com>
2025-06-23 09:23:23 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,6 +1,7 @@
 variable "project_id" {
-  description = "GCP Project ID"
+  description = "GCP project ID"
   type        = string
+  default     = "graphite-store-463414-p2"
 }
 
 variable "region" {
@@ -15,31 +16,8 @@ variable "zone" {
   default     = "us-central1-a"
 }
 
-variable "vpc_name" {
-  description = "VPC name"
-  type        = string
-  default     = "my-vpc"
-}
-
-variable "public_subnet_cidr" {
-  description = "Public subnet CIDR"
-  type        = string
-  default     = "11.0.1.0/24"
-}
-
-variable "private_subnet_cidr" {
-  description = "Private subnet CIDR"
-  type        = string
-  default     = "10.0.2.0/24"
-}
-
-variable "machine_type" {
-  description = "GCE machine type"
-  type        = string
-  default     = "e2-medium"
-}
-
 variable "email_alert" {
-  description = "Email for alert notifications"
+  description = "Email for CPU alert notifications"
   type        = string
+  default     = "ashweryaverma@gmail.com"
 }
