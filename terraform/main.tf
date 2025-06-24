be095b7d380f7f7bbb3823b31395399f9e7831b6
ash03lko <ashweryaverma@gmail.com>
2025-06-24 08:28:17 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,7 +1,7 @@
 provider "google" {
-  project = "graphite-store-463414-p2"
-  region  = "us-central1"
-  zone    = "us-central1-a"
+  project = var.project_id
+  region  = var.region
+  zone    = var.zone
 }
 
 # VPC
@@ -14,22 +14,22 @@ resource "google_compute_subnetwork" "public" {
   name          = "public-subnet"
   network       = google_compute_network.vpc.id
   ip_cidr_range = "10.0.1.0/24"
-  region        = "us-central1"
+  region        = var.region
 }
 
 # Private subnet
 resource "google_compute_subnetwork" "private" {
-  name          = "private-subnet"
-  network       = google_compute_network.vpc.id
-  ip_cidr_range = "10.0.2.0/24"
-  region        = "us-central1"
+  name                   = "private-subnet"
+  network                = google_compute_network.vpc.id
+  ip_cidr_range          = "10.0.2.0/24"
+  region                 = var.region
   private_ip_google_access = true
 }
 
 # Static external IP
 resource "google_compute_address" "web_static_ip" {
   name   = "web-static-ip"
-  region = "us-central1"
+  region = var.region
 }
 
 # Firewall for HTTP/HTTPS
@@ -46,11 +46,11 @@ resource "google_compute_firewall" "allow_http_https" {
   direction     = "INGRESS"
 }
 
-# VM
+# Compute Engine instance
 resource "google_compute_instance" "web_server" {
   name         = "web-server"
   machine_type = "e2-micro"
-  zone         = "us-central1-a"
+  zone         = var.zone
 
   boot_disk {
     initialize_params {
@@ -76,11 +76,20 @@ resource "google_compute_instance" "web_server" {
   EOT
 }
 
-# Monitoring alert
+# Email Notification Channel
+resource "google_monitoring_notification_channel" "email_channel" {
+  display_name = "Email Notifications"
+  type         = "email"
+  labels = {
+    email_address = var.email_alert
+  }
+}
+
+# CPU alert
 resource "google_monitoring_alert_policy" "cpu_alert" {
   display_name = "High CPU Alert"
   combiner     = "OR"
-  notification_channels = [] # Add channels later if needed
+  notification_channels = [google_monitoring_notification_channel.email_channel.id]
 
   conditions {
     display_name = "VM CPU > 80%"
@@ -97,6 +106,7 @@ resource "google_monitoring_alert_policy" "cpu_alert" {
   }
 }
 
+# Output static IP
 output "static_ip" {
   value = google_compute_address.web_static_ip.address
 }
